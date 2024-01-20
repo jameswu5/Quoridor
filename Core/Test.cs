93b80b69dfe5479299fc57684589fbead0b14d80
James Wu <jameswu144@gmail.com>
2024-01-20 20:31:05 +0000

    test module

---
+++ Core/Test.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Quoridor;
+
+public static class Test
+{
+    /// <summary>
+    /// Returns the number of leaf nodes at a depth given a board
+    /// </summary>
+    public static int Perft(Board board, int depth)
+    {
+        if (depth == 1)
+        {
+            return board.legalMoves.Count;
+        }
+
+        int numOfPositions = 0;
+
+        foreach (int move in board.legalMoves)
+        {
+            board.MakeMove(move);
+            numOfPositions += Perft(board, depth - 1);
+            board.UndoMove();
+        }
+
+        return numOfPositions;
+    }
+
+    /// <summary>
+    /// Makes 'numOfMoves' moves on random then unmakes them all (going back to original position)
+    /// </summary>
+    public static void Shuffle(Board board, int numOfMoves)
+    {
+        Random rng = new();
+
+        for (int i = 0; i < numOfMoves; i++)
+        {
+            int index = rng.Next(board.legalMoves.Count);
+            board.MakeMove(board.legalMoves[index]);
+        }
+        for (int i = 0; i < numOfMoves; i++)
+        {
+            board.UndoMove();
+        }
+    }
+
+    public static void TestMoveGeneration(int iterations, int numOfMoves, int depth)
+    {
+        for (int i = 0; i < iterations; i++)
+        {
+            Board board = new();
+            Shuffle(board, numOfMoves);
+            Console.WriteLine(Perft(board, depth));
+        }
+    }
+}
